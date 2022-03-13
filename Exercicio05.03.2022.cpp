7d94596e50e192b600d02b249e736fe1f0ee180c
Maria Clara <70076859+Mariars11@users.noreply.github.com>
2022-03-13 12:30:54 -0300

    Add files via upload

---
+++ Exercicio05.03.2022.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+
+int main(){
+	//permite o uso de acentuação no printf
+	setlocale(LC_ALL, "Portuguese");
+	
+	//Declaração de variáveis
+	int num1, num2, resultado, aux;
+	float raiz;
+	
+	do{
+		//Informar os numeros a serem calculados
+		//entrada de dados
+		printf("\n");
+		printf("\n=== Informe os números e as operações desejadas, conforme solicitado! === \n");
+		printf("\nDigite o primeiro número: ");
+		scanf("%d", &num1);
+		printf("Digite o segundo número: ");
+		scanf("%d", &num2);
+		
+		//switch case e as operações
+		printf("|1| - soma dos números\n|2| - produto do primeiro número pelo quadrado do segundo\n|3| - o quadrado do primeiro número\n|4| - A raiz quadrada da soma dos quadrados ");
+		printf("\nEscolha a opção: ");
+		scanf("%d", &aux);
+		switch(aux){
+			case 1:
+				resultado = num1 + num2;
+				printf("\nO resultado: %d", resultado);
+				break;
+			case 2:
+				resultado = num1 * (num2 * num2);
+				printf("\nO resultado: %d", resultado);
+				break;
+			case 3:
+				resultado = (num1 * num1);
+				printf("\nO resultado: %d", resultado);
+				break;
+			case 4:
+				resultado = (num1 * num1) + (num2 * num2);
+				raiz = sqrt(resultado);
+				printf("\nO resultado: %.2f", raiz);
+				break;
+			default:
+				printf("Essa opção não está disponível");
+		}
+	}while(aux != 0);	
+}
+
