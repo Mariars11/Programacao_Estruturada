b5b2c7324ea263211928459bb25facecbcd0dae4
Maria Clara <70076859+Mariars11@users.noreply.github.com>
2022-04-28 17:58:33 -0300

    Update matriz_valores.cpp

--- matriz_valores.cpp
+++ matriz_valores.cpp
@@ -4,10 +4,10 @@
 
 int main(){
 	int i, j, valores[lin][col] = 	{ 	{5,3,10,14},
-										{7,1,11,15},
-										{8,2,12,17},
-										{9,6,13,18}
-									};
+						{7,1,11,15},
+						{8,2,12,17},
+						{9,6,13,18}
+					};
 	int soma_linhas[lin] = {0}, multiplicacao_colunas[col] = {1,1,1,1}; 
 	// Soma das linhas
 	for(i = 0; i < lin; i++){
@@ -15,13 +15,13 @@ int main(){
 			soma_linhas[i] += valores[i][j];
 		}		
 	}
-	// Multiplicação das colunas	
+	// MultiplicaÃ§Ã£o das colunas	
 	for(i = 0; i < lin; i++){
 		for(j = 0; j < col; j++){
 			multiplicacao_colunas[j] *= valores[i][j];
 		}		
 	}
-	// Impressão da matriz e da soma das linhas	
+	// ImpressÃ£o da matriz e da soma das linhas	
 	for(i = 0; i < lin; i++){
 		for(j = 0; j < col; j++){
 			printf("\t%4d", valores[i][j]);
@@ -34,7 +34,7 @@ int main(){
 	printf("\t   *\t   *\t  *\t   *");
 	printf("\n");
 	
-	// Impressão da multiplicação das colunas
+	// ImpressÃ£o da multiplicaÃ§Ã£o das colunas
 		
 	for(j = 0; j < col; j++){
 		printf("\t %4d", multiplicacao_colunas[j]);
